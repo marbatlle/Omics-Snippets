fa849f4e7d973a6255dd0c48159fe5a9f65144ad
Mar Batlle <marbatlleperez@gmail.com>
2021-01-22 12:56:47 +0100

    Add fastqc script

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -2,12 +2,15 @@ echo "*** INITIATING PIPELINE ***"
 
 echo "* INITIATING Quality Control *"
 mkdir -p out/fastqc
-for sid in $(ls raw_data/*.fastq  | cut -d"." -f1 | sed "s:raw_data/::")
+for sid in $(ls raw_data/*.fastq | cut -d"." -f1 | sed "s:raw_data/::")
 do
-    if [! -f out/fastqc/${sid}_fastqc.html]
-then
-    ls raw_data/*.fastq
-    fastqc raw_data/WEx_Normal_R1.fastq --outdir=out/fastqc
-echo "* Quality Control FINALIZED *"
+    if [ ! -f out/fastqc/${sid}_fastqc.html ]
+    then
+        fastqc raw_data/${sid}.fastq --outdir=out/fastqc
+        echo "* Quality Control FINALIZED *"
+    else
+        echo "* PROCESS EXITED. Sample $sid Quality Control OUTPUT ALREADY EXISTS *"
+    fi
+done
 
 echo "*** PIPELINE FINALIZED ***"
